c24662b0fb32562d7f8726c5f73fb4c9aba20f33
tofilagman <tofilagman@gmail.com>
2025-11-13 20:09:38 +0800

    fixed monaco ctrl+v

--- report-designer/src/main.js
+++ report-designer/src/main.js
@@ -137,3 +137,38 @@ ipcMain.handle('syncLib', async (event, url) => {
         throw new Error(`${ex.response.status}: ${ex.response.data}`);
     }
 });
+
+/**
+ * fixed for monaco editor ctrl+v 
+ * https://github.com/microsoft/monaco-editor/issues/4855#issuecomment-3184259279
+ */
+const isMac = process.platform === "darwin";
+app.on("browser-window-created", (_, win) => {
+    win.webContents.on("before-input-event", (event, input) => {
+        const isCmdOrCtrl = isMac ? input.meta === true : input.control === true;
+
+        const hasShift =
+            input.shift === true ||
+            input.modifiers.includes("shift");
+
+        const hasAlt =
+            input.alt === true ||
+            input.modifiers.includes("alt");
+
+        // Prefer code (layout-agnostic)
+        const isV = input.code === "KeyV" || input.key === "v";
+
+        const shouldPaste =
+            input.type === 'keyDown' &&
+            isCmdOrCtrl &&
+            !hasShift &&
+            !hasAlt &&
+            isV;
+
+        if (shouldPaste) {
+            // Native paste path (works with Monaco)
+            win.webContents.paste();
+            event.preventDefault();
+        }
+    });
+});
